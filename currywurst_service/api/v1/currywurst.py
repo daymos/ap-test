2639a76e31646634f7e07485a3ec01b0aa66a955
mattia spinelli <mattiaspinelli@mattias-MacBook-Air.local>
2023-03-10 11:02:01 +0000

    pubblish event

--- currywurst_service/api/v1/currywurst.py
+++ currywurst_service/api/v1/currywurst.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter,  HTTPException
+from fastapi import APIRouter,  HTTPException, Request
 
 from currywurst_service.schemas.currywurst import currywurstPurchaseRequest, currywurstPurchaseResponse
 from currywurst_service.service.currywurst_service import return_coins, is_price_met
+from currywurst_service.service.event_service import publish_event
 
 
 router = APIRouter()
@@ -13,14 +14,15 @@ def introduce_yourself():
 
 
 @router.post("/currywurst/pay")
-async def currywurst_pay(request: currywurstPurchaseRequest):
-    shoud_proceed = is_price_met(request.eur_inserted, request.currywurst_price)
+async def currywurst_pay(currywurstPurchaseRequest: currywurstPurchaseRequest, request: Request):
+    shoud_proceed = is_price_met(currywurstPurchaseRequest.eur_inserted, currywurstPurchaseRequest.currywurst_price)
 
     if not shoud_proceed:
         raise HTTPException(status_code = 400, detail =  "Need more money.")
 
-    change = return_coins(request.eur_inserted, request.currywurst_price) 
+    change = return_coins(currywurstPurchaseRequest.eur_inserted, currywurstPurchaseRequest.currywurst_price) 
 
     # await publish method
+    await publish_event(request.app.state.mb, change, 'test')
     
     return change
--- currywurst_service/service/event_service.py
+++ currywurst_service/service/event_service.py
@@ -8,22 +8,11 @@ async def create_connection():
     )
     return connection
 
+async def publish_event(connection, msg, routing_key):
+    async with connection:
+        channel = await connection.channel()
 
-
-#class Rabbit_MQ_Client():
-#    async def create_connection(self):
-#        self.connection = await aio_pika.connect_robust(
-#        "amqp://guest:guest@127.0.0.1/",
-#    )
-#
-#    async def publish_event(self, routing_key, msg) -> None:
-#
-#        async with self.connection:
-#
-#            channel = await self.connection.channel()
-#
-#            await channel.default_exchange.publish(
-#                aio_pika.Message(body=json.dumps(msg).encode()),
-#                routing_key=routing_key,
-#            )
-
+        await channel.default_exchange.publish(                     
+            aio_pika.Message(body=json.dumps(msg).encode()),        
+            routing_key=routing_key,                                
+        )
